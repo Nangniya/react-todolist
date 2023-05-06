25590a7a5cc26f7156ff750987217fe865a940a7
[SW4/2반] 김나연 <apinksky00e@gmail.com>
2023-05-06 15:29:52 +0900

    fix: ref 사용하는 것으로 수정

--- src/components/TodoAdd.jsx
+++ src/components/TodoAdd.jsx
@@ -1,29 +1,22 @@
-import {useState} from 'react';
+import {useRef} from 'react';
 
-const TodoAdd = ({todos, setTodos}) => {
+const TodoAdd = ({setTodos}) => {
 
-    const [value, setValue] = useState('');
-    const onChange = (e) => { setValue(e.target.value); };
-    const onClick = (e) => { 
-        let copy = [...todos];
-        copy.push(value);
-        setTodos(copy);
-        setValue(''); };
-    const onKeyDown = (e) => {
-        if (e.key === 'Enter') { onClick(); }
-    }
+    const newTodoRef = useRef();
+    const onSubmit= (e) => { 
+        e.preventDefault();
+        const newTodoText = newTodoRef.current.value;
+        setTodos((prev) => [...prev, newTodoText]);
+        newTodoRef.current.value = '';    
+    };
 
     return (
-        <div>
+        <form onSubmit={onSubmit}>
           <input
-          onChange={onChange}
-          onKeyDown={onKeyDown}
-          placeholder="할 일을 입력하세요"
-          value={value} />
-          <button 
-          type='submit'
-          onClick={onClick}>+</button>
-        </div>
+          ref = {newTodoRef}
+          placeholder="할 일을 입력하세요"/>
+          <button type='submit'>+</button>
+        </form>
     )
 }
 
