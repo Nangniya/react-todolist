a39574c3aa257bf48b46355bebe1df64564bfcc8
[SW4/2반] 김나연 <apinksky00e@gmail.com>
2023-05-09 11:57:39 +0900

    feat: 수정 기능 구현

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,33 +1,67 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React, { useRef } from "react";
+import styled from "styled-components";
 
 const StyledLi = styled.li`
   list-style: none;
 `;
 const StyledSpan = styled.span`
-  text-decoration: ${props => props.checked ? 'line-through' : 'none'};
+  text-decoration: ${(props) => (props.checked ? "line-through" : "none")};
 `; //조건부 스타일 적용
 
-const Todo = ({todo, setTodos}) => {
-    const deleteTodo = () => {
-        setTodos(prev => prev.filter(item => item.id !== todo.id));
-      };
-    const checkTodo = () => {
-      setTodos(prev => prev.map(item => {
-        if(item.id === todo.id) {
-          return {...item, checked: !item.checked}
+const Todo = ({ todo, setTodos }) => {
+  const deleteTodo = () => {
+    setTodos((prev) => prev.filter((item) => item.id !== todo.id));
+  };
+  const editRef = useRef();
+  const handleBlur = () => {
+    setTodos((prev) => {
+      const newTodos = prev.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, text: editRef.current.value };
         }
         return item;
-      }))
-    }
-    return (
-        <StyledLi>
-            <span onClick={checkTodo}>{!todo.checked ? '🥚' : '🐣'}
-            <StyledSpan checked={todo.checked}>{todo.text}</StyledSpan></span>
-            <button onClick={deleteTodo}>삭제</button>
-        </StyledLi>
+      });
+      return newTodos;
+    });
+    editRef.current.blur(); // blur 이벤트 호출
+    setEdit();
+  };
+  const setEdit = () => {
+    setTodos((prev) =>
+      prev.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, edit: !item.edit };
+        }
+        return item;
+      })
     );
+  };
+  const checkTodo = () => {
+    setTodos((prev) =>
+      prev.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, checked: !item.checked };
+        }
+        return item;
+      })
+    );
+  };
+  return (
+    <StyledLi>
+      <span onClick={checkTodo}>{!todo.checked ? "🥚" : "🐣"}</span>
+      {!todo.edit ? (
+        <StyledSpan checked={todo.checked}>{todo.text}</StyledSpan>
+      ) : (
+        <input
+          ref={editRef}
+          defaultValue={todo.text}
+          onBlur={handleBlur}
+        ></input>
+      )}
+      <button onClick={setEdit}>수정</button>
+      <button onClick={deleteTodo}>삭제</button>
+    </StyledLi>
+  );
 };
 
 export default Todo;
--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,19 +1,22 @@
 let data = [
-    {
+  {
     id: 1,
-    text: '할일1',
-    checked: false
-    },
-    {
+    text: "할일1",
+    checked: false,
+    edit: false,
+  },
+  {
     id: 2,
-    text: '할일2',
-    checked: false
-    },
-    {
+    text: "할일2",
+    checked: false,
+    edit: false,
+  },
+  {
     id: 3,
-    text: '할일3',
-    checked: false
-    }
+    text: "할일3",
+    checked: false,
+    edit: false,
+  },
 ];
 
 export default data;
